cfbeb4dfd7b146b5792dc0357485ea7cdd21c91c
Noah Manger <noah.manger@gsa.gov>
2015-08-20 19:54:07 -0700

    Adding browserify for style guide js

---
+++ fec-template/public/init.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/* global require */
+
+var $ = require('jquery');
+var accordion = require('../../js/accordion.js');
+var glossary = require('../../js/glossary.js');
+var dropdown = require('../../js/dropdowns.js');
+
+var SLT_ACCORDION = '.js-accordion';
+
+$(SLT_ACCORDION).each(function() {
+  Object.create(accordion).init($(this));
+});
+
+$('.js-checkbox-filters').each(function() {
+  new dropdown.Dropdown(this);
+})
