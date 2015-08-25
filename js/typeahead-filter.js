cf36fa23764915fcf76489304696dcc5965e024f
Noah Manger <noah.manger@gsa.gov>
2015-08-24 23:07:35 -0700

    Look up selected committees and load

--- js/typeahead-filter.js
+++ js/typeahead-filter.js
@@ -1,29 +1,40 @@
 'use strict';
 
-/* global require, module, window, document */
+/* global require, module, window, document, API_LOCATION, API_VERSION, API_KEY */
 
 var $ = require('jquery');
+var URI = require('URIjs');
+var _ = require('underscore');
 
 var events = require('./events');
 
 var TypeaheadFilter = function(selector, dataset) {
   var self = this;
-  var dataset = dataset;
 
   self.$body = $(selector);
-
+  
   self.$field = self.$body.find('input[type="text"]');
   self.fieldName = self.$field.attr('name');
+  self.dataset = dataset;
   self.$selected = self.$body.find('.dropdown__selected');
   self.$field.on('typeahead:selected', this.handleSelect.bind(this));
-  self.$field.typeahead({}, dataset);
-}
+  self.$field.typeahead({}, this.dataset);
+  $(window).on('load', this.getFilters.bind(this));
+};
 
 TypeaheadFilter.prototype.handleSelect = function(e, datum) {
   var name = this.fieldName;
   var label = e.currentTarget.value;
   var value = datum.id;
   var id = value + '-checkbox';
+  this.appendField(name, id, value, label)
+};
+
+TypeaheadFilter.prototype.clearInput = function() {
+  this.$field.typeahead('val', null);
+};
+
+TypeaheadFilter.prototype.appendField = function(name, id, value, label) {
   this.$selected
     .append('<li><input ' + 
             ' name="' + name + '"' +
@@ -33,11 +44,27 @@ TypeaheadFilter.prototype.handleSelect = function(e, datum) {
             '<label for="' + id + '">' + label + '</li>');
   this.$field.val(id).change();
   this.clearInput();
-}
+};
 
-TypeaheadFilter.prototype.clearInput = function() {
-  this.$field.typeahead('val', null);
-}
+TypeaheadFilter.prototype.getFilters = function() {
+  var self = this;
+  var ids = this.$field.val().split(',');
+  var dataset = this.dataset.name + 's';
+  if ( ids.length ) {
+    _.each(ids, function(id) {
+      $.getJSON(
+          URI(API_LOCATION)
+          .path([API_VERSION, 'names', dataset].join('/'))
+          .addQuery('api_key', API_KEY)
+          .addQuery('q', id)
+          .toString())
+          .done(function(response) {
+            var name = response.results[0].name;
+            self.appendField('committee_id', id + '-checkbox', id, name);
+          })
+    })
+  }
+};
 
 module.exports = {
   TypeaheadFilter: TypeaheadFilter,
