e13db6043fcb232320d48da0f4e841c98be3d247
Noah Manger <noah.manger@gsa.gov>
2015-09-23 14:43:01 -0700

    Implementing skip nav

---
+++ js/skip-nav.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/* global require, module, document */
+
+var $ = require('jquery');
+
+function Skipnav(selector) {
+  var self = this;
+  self.$link = $(selector);
+  self.$target = self.findTarget();
+  $(document.body).on('click, keyup', self, this.focusOnTarget.bind(this));
+};
+
+Skipnav.prototype.findTarget = function() {
+  return $('main').find('a, button, :input, [tabindex]').filter(':visible')[0];
+};
+
+Skipnav.prototype.focusOnTarget = function(e) {
+  if (e.keyCode === 13 || e.type === 'click') {    
+    this.$target.focus();
+  }
+};
