588282f8d4ee9ab474eccaef0ec753a8c3213a29
Noah Manger <noah.manger@gsa.gov>
2015-10-12 11:29:55 -0700

    Styling and rewriting status messages
    We were running out of height on the form when the status message
    shows, so I set it to hide the form and added a button to show it
    again.

--- js/feedback.js
+++ js/feedback.js
@@ -5,8 +5,8 @@ var _ = require('underscore');
 var feedback = require('./templates/feedback.html');
 
 var statusClasses = {
-  success: 'success',
-  error: 'error'
+  success: 'message--success',
+  error: 'message--error'
 };
 
 /**
@@ -22,11 +22,13 @@ function Feedback(url, parent) {
   $(parent || 'body').append(this.$feedback);
 
   this.$button = this.$feedback.find('.js-feedback');
+  this.$reset = this.$feedback.find('.js-reset');
   this.$box = this.$feedback.find('.js-feedback-box');
   this.$status = this.$box.find('.js-status');
   this.$form = this.$feedback.find('form');
 
   this.$button.on('click', this.toggle.bind(this));
+  this.$reset.on('click', this.reset.bind(this));
   this.$form.on('submit', this.submit.bind(this));
 }
 
@@ -66,22 +68,40 @@ Feedback.prototype.submit = function(e) {
   promise.fail(this.handleError.bind(this));
 };
 
-Feedback.prototype.handleSuccess = function() {
+Feedback.prototype.handleSuccess = function(e) {
+  e.preventDefault();
+  var message = 
+    '<h2>Thank you for helping us improve betaFEC</h2>' +
+    '<p>This information has been reported on GitHub, where it is publicly visible.' +
+    'You can review all reported feedback on <a href="https://github.com/18f/fec/issues">our GitHub page</a>.</p>';
+  var buttonText = 'Submit another issue';
   this.$box.find('textarea').val('');
-  this.message('Issue submitted.', 'success');
+  this.message(message, buttonText, 'success');
 };
 
 Feedback.prototype.handleError = function() {
-  this.message('Error submitting issue. Please try again.', 'error');
+  var message =
+    '<h2>There was an error</h2>' +
+    '<p>Please try submitting your issue again.</p>';
+  var buttonText = 'Try again';
+  this.message(message, buttonText, 'error');
 };
 
-Feedback.prototype.message = function(text, style) {
+Feedback.prototype.message = function(text, buttonText, style) {
   var self = this;
-  self.$status.text(text);
+  this.$form.attr('aria-hidden', true);
+  self.$status.attr('aria-hidden', false);
+  self.$reset.text(buttonText);
+  self.$status.find('.js-status-content').html(text);
   _.each(statusClasses, function(value) {
     self.$status.removeClass(value);
   });
   self.$status.addClass(statusClasses[style]);
+};
+
+Feedback.prototype.reset = function() {
+  this.$form.attr('aria-hidden', false);
+  this.$status.attr('aria-hidden', true);
 };
 
 module.exports = {Feedback: Feedback};
