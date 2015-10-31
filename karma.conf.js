4af2a38ecadf26ecbaf5d32e2a5a7880416453a3
Joshua Carp <jm.carp@gmail.com>
2015-10-31 17:52:01 -0400

    Use karma test runner.
    Karma is a better-supported and fuller-featured test runner than
    mochify. It's also not tied to browserify, which would smooth the
    transition to another build tool should we go in that direction.

---
+++ karma.conf.js
@@ -0,0 +1,42 @@
+var istanbul = require('browserify-istanbul');
+
+module.exports = function(config) {
+  config.set({
+    frameworks: ['browserify', 'mocha', 'chai-sinon'],
+
+    files: [
+      'tests/**/*.js'
+    ],
+
+    preprocessors: {
+      'js/**/*.js': ['browserify'],
+      'tests/**/*.js': ['browserify']
+    },
+
+    browserify: {
+      debug: true,
+      transform: [
+        'stringify',
+        istanbul({
+          ignore: [
+            'tests/unit/**',
+            '**/templates/**'
+          ]
+        })
+      ]
+    },
+
+    coverageReporter: {
+      subdir: '.',
+      reporters: [
+        {type: 'html'},
+        {type: 'text'},
+        {type: 'json', file: 'coverage.json'}
+      ]
+    },
+
+    reporters: ['progress', 'coverage'],
+    browsers: ['PhantomJS2'],
+    port: 9876
+  });
+};
