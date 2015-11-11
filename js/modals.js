6b73e85f1b51ae5db05e2fab7258d98084f1c200
Noah Manger <noah.manger@gsa.gov>
2015-11-10 18:00:22 -0800

    adding missing semicolons

--- js/modals.js
+++ js/modals.js
@@ -9,14 +9,14 @@ function Modal(trigger, parent) {
 
   $('body').on('click', '.js-modal', this.addModal.bind(this));
   $('body').on('click', '.js-close', this.destroyModal.bind(this));
-}
+};
 
 Modal.prototype.addModal = function() {
   this.$parent.append(this.$body);
-}
+};
 
 Modal.prototype.destroyModal = function() {
   this.$body.remove();
-}
+};
 
 module.exports = {Modal: Modal};
