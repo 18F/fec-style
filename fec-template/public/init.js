1fffae4fe2dd05c2a210b0c38c3eb4e6a4ca70fd
Joshua Carp <jm.carp@gmail.com>
2016-02-28 13:58:15 -0500

    Delete feedback module in favor of external feedback tool.

--- fec-template/public/init.js
+++ fec-template/public/init.js
@@ -1,7 +1,5 @@
 'use strict';
 
-/* global require, window, document */
-
 var $ = require('jquery');
 
 // Hack: Append jQuery to `window` for use by legacy libraries
@@ -13,7 +11,6 @@ var dropdown = require('../../js/dropdowns');
 var siteNav = require('../../js/site-nav');
 var typeahead = require('../../js/typeahead');
 var typeaheadFilter = require('../../js/typeahead-filter');
-var feedback = require('../../js/feedback');
 
 var SLT_ACCORDION = '.js-accordion';
 
@@ -28,8 +25,6 @@ $('.js-dropdown').each(function() {
 $('.js-site-nav').each(function() {
   new siteNav.SiteNav(this);
 });
-
-new feedback.Feedback();
 
 new typeahead.Typeahead('.js-search-input', $('.js-search-type').val());
 
