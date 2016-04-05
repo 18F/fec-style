63b29a8335a98f9ac0b7cf7e8df97ec876e8d5ff
Noah Manger <noah.manger@gsa.gov>
2016-04-04 23:37:14 -0700

    Updating styleguide basics

--- fec-template/public/init.js
+++ fec-template/public/init.js
@@ -28,4 +28,38 @@ new typeahead.Typeahead('.js-search-input', $('.js-search-type').val());
 
 new typeaheadFilter.TypeaheadFilter('.js-typeahead-filter', typeahead.datasets.committees);
 
-new accordion.Accordion();
+$('.js-accordion').each(function(){
+  var contentPrefix = $(this).data('content-prefix') || 'accordion';
+  var openFirst = $(this).data('open-first') || false;
+  var opts = {
+    contentPrefix: contentPrefix,
+    openFirst: openFirst
+  };
+  new accordion.Accordion({}, opts);
+});
+
+// Styleguide specific stuff
+
+function MarkupPanel($wrapper) {
+  this.$wrapper = $wrapper;
+  this.$button = $wrapper.find('.js-toggle-markup');
+  this.$markup = this.$wrapper.next('.kss-markup');
+  this.markupVisible = false;
+  this.$button.on('click', this.toggleMarkup.bind(this));
+}
+
+MarkupPanel.prototype.toggleMarkup = function() {
+  if (this.markupVisible) {
+    this.$markup.hide().attr('aria-hidden', 'true');
+    this.markupVisible = false;
+    this.$button.text('Show markup');
+  } else {
+    this.$markup.show().attr('aria-hidden', 'false');
+    this.markupVisible = true;
+    this.$button.text('Hide markup');
+  }
+};
+
+$('.kss-modifier__wrapper').each(function(){
+  new MarkupPanel($(this));
+});
