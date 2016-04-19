623ede40df791601a545e3f442c4e7653d90d596
Noah Manger <noah.manger@gsa.gov>
2016-04-19 16:43:55 -0700

    Base64 encoding icon font

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 var iconfont = require('gulp-iconfont');
 var consolidate = require('gulp-consolidate');
 var rename = require('gulp-rename');
-var cssfont64 = require('gulp-cssfont64');
+var cssBase64 = require('gulp-css-base64');
 
 var FONTNAME = 'fec-icons';
 var CLASSNAME = 'i';
@@ -26,10 +26,19 @@ gulp.task('build-icons', function () {
         return { name: glyph.name, codepoint: glyph.unicode[0].charCodeAt(0) }
       })
     };
+    // Generate the .scss file
     gulp.src('fec-icons/icon-template.scss')
       .pipe(consolidate('underscore', options))
-      .pipe(rename({basename: '_icons', extname: '.scss'}))
+      .pipe(rename({basename: '_icons'}))
       .pipe(gulp.dest('./scss/'));
   })
   .pipe(gulp.dest('./fonts/'))
+});
+
+gulp.task('encode', function() {
+  return gulp.src('fec-icons/_fec-icon-font.scss')
+    .pipe(cssBase64({
+      extensionsAllowed: ['.woff', '.woff2']
+    }))
+    .pipe(gulp.dest('scss'));
 });
