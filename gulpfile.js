88a6cf4e2d2e8431a18f503ad876c5578ab67541
Noah Manger <noah.manger@gsa.gov>
2016-04-25 18:17:07 -0700

    Replacing with inline svgs

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,56 @@
 var _ = require('underscore');
+var fs = require('fs');
 var gulp = require('gulp');
-var iconfont = require('gulp-iconfont');
 var consolidate = require('gulp-consolidate');
 var rename = require('gulp-rename');
-var cssBase64 = require('gulp-css-base64');
+var svgmin = require('gulp-svgmin');
+var urlencode = require('gulp-css-urlencode-inline-svgs');
 
-var FONTNAME = 'fec-icons';
-var CLASSNAME = 'i';
-var TIMESTAMP = Math.round(Date.now() / 1000);
+function writeScss(stream, file) {
 
-gulp.task('build-icons', function () {
- return gulp.src('fec-icons/icons/*.svg')
-  .pipe(iconfont({
-    fontName: FONTNAME,
-    prependUnicode: false,
-    formats: ['ttf', 'eot', 'woff', 'woff2'],
-    timestamp: TIMESTAMP
-  }))
-  .on('glyphs', function(glyphs) {
-    var options = {
-      fontName: FONTNAME,
-      fontPath: '../fonts/',
-      className: CLASSNAME,
-      glyphs: _.map(glyphs, function(glyph) {
-        return { name: glyph.name, codepoint: glyph.unicode[0].charCodeAt(0) }
-      })
-    };
-    gulp.src('fec-icons/icon-template.scss')
-      .pipe(consolidate('underscore', options))
-      .pipe(rename({basename: '_icons'}))
-      .pipe(gulp.dest('./scss/'));
-  })
-  .pipe(gulp.dest('./fonts/'))
-});
+}
 
-gulp.task('encode', function() {
-  return gulp.src('fec-icons/_fec-icon-font.scss')
-    .pipe(cssBase64({
-      extensionsAllowed: ['.woff', '.woff2']
+gulp.task('minify', function() {
+  return gulp.src('./fec-icons/icons/*.svg')
+    .pipe(svgmin({
+      plugins: [
+        {
+          removeDimensions: true
+        },
+        {
+          removeAttrs: {attrs: '(fill|fill-rule)'}
+        }
+      ]
     }))
-    .pipe(gulp.dest('scss'));
+    .pipe(gulp.dest('./img/icons'));
 });
+
+gulp.task('consolidate', function() {
+  function getSVGs() {
+    var svgs = [],
+        files = fs.readdirSync('./img/icons/');
+    files = _.filter(files, function(file) {
+      return file.substr(-4) === '.svg'
+    });
+    _.map(files, function(file) {
+      var data = fs.readFileSync('./img/icons/' + file, 'utf8');
+      svgs.push({
+        name: file.split('.')[0],
+        content: data,
+      });
+    });
+    return svgs;
+  };
+
+  var svgs = getSVGs();
+  var data = {
+    icons: svgs
+  }
+  return gulp.src('./fec-icons/icon-template.scss')
+    .pipe(consolidate('underscore', data))
+    .pipe(rename({basename: '_icon-variables'}))
+    .pipe(urlencode())
+    .pipe(gulp.dest('./scss/'))
+    .pipe(gulp.dest('./'))
+});
+
