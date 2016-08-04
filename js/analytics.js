f98ae27e46e1e6887241a044955457296e088690
Noah Manger <noah.manger@gsa.gov>
2016-08-04 16:13:58 -0700

    Fixing analytics tracker

--- js/analytics.js
+++ js/analytics.js
@@ -15,7 +15,7 @@ function trackerExists() {
  * This tracker's name is "nonDAP", so all commands will need to be prefixed
 */
 function init() {
-  if (!trackerExists) {
+  if (!trackerExists()) {
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
