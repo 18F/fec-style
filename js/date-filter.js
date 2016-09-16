47a54df6d45e69c9902f2a02ebd1efbde5f439f3
xtine <christine@fieryorange.com>
2016-09-15 22:02:29 -0700

    selecting end date gets last day of month

--- js/date-filter.js
+++ js/date-filter.js
@@ -254,14 +254,20 @@ DateFilter.prototype.handleMaxDateSelect = function() {
 DateFilter.prototype.handleGridItemSelect = function(e) {
   var value = [];
   var $selectDate = $(e.target).parent();
+  var $selectDateMonth = $selectDate.data('month');
+  var $selectDateYear = $selectDate.parent().attr('data-year');
 
   if (this.$grid.hasClass('pick-min')) {
-    value[0] = $selectDate.data('month') + '/01/' + $selectDate.parent().attr('data-year');
+    value[0] = $selectDateMonth + '/01/' + $selectDateYear;
     value[1] = this.$maxDate.val();
   }
   else {
+    // calculate last day of month for end date
+    var lastDay = new Date($selectDateYear, $selectDateMonth, 0);
+    lastDay = lastDay.getDate();
+
     value[0] = this.$minDate.val();
-    value[1] = $selectDate.data('month') + '/01/' + $selectDate.parent().attr('data-year');
+    value[1] = $selectDateMonth + '/' + lastDay +'/' + $selectDateYear;
   }
 
   if (!this.$grid.hasClass('is-invalid')) {
