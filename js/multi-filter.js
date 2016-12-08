d6bc984953494da574da5737e6905d99c5e51634
Noah Manger <noah.manger@gsa.gov>
2016-12-07 16:27:50 -0800

    Activating subfilters

--- js/multi-filter.js
+++ js/multi-filter.js
@@ -10,18 +10,31 @@ var CheckboxFilter = require('./checkbox-filter').CheckboxFilter;
 
 function MultiFilter(elm) {
   Filter.call(this, elm);
-  this.$subfilters = this.$elm.find('.js-sub-filter[data-name="' + this.name + '"]');
-
-  this.$subfilters.each(function() {
-    var subfilter = new CheckboxFilter(this);
-    // Explicitly assign filterLabel, which will show the count
-    // Necessary because each subfilter may be part of a different accordion
-    subfilter.$filterLabel = $('#' + subfilter.$elm.data('filter-label'));
-  });
+  this.subfilters = this.activateSubfilters();
 }
 
 MultiFilter.prototype = Object.create(Filter.prototype);
 MultiFilter.constructor = MultiFilter;
+
+MultiFilter.prototype.activateSubfilters = function() {
+  var subfilters = [];
+  // Activate each sub-filter and add it to an array
+  this.$elm.find('.js-sub-filter[data-name="' + this.name + '"]').each(function() {
+    var subfilter = new CheckboxFilter(this);
+    // Explicitly assign filterLabel, which will show the count
+    // Necessary because each subfilter may be part of a different accordion
+    subfilter.$filterLabel = $('#' + subfilter.$elm.data('filter-label'));
+    subfilters.push(subfilter);
+  });
+
+  return subfilters;
+};
+
+MultiFilter.prototype.fromQuery = function(query) {
+  this.subfilters.forEach(function(filter) {
+    filter.fromQuery(query);
+  });
+};
 
 module.exports = {
   MultiFilter: MultiFilter
