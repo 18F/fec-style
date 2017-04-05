f0fd89e51851c4a8b99ccb69a53f318f4fb5ef2c
xtine <christine@fieryorange.com>
2017-04-05 13:03:32 -0600

    days countdown timer

--- js/site-orientation.js
+++ js/site-orientation.js
@@ -1,21 +1,27 @@
 'use strict';
 
 var $ = require('jquery');
+var countdown = require('countdown');
 
 function SiteOrientation(selector) {
   this.$selector = $(selector);
 
-  this.$toggle = this.$selector.find('.toggle-text');
+  // fill in _ DAYS until this site...
+  this.$days = this.$selector.find('.days');
+  var countdownDate = new Date(this.$days.data('date'));
+  this.$days.html(countdown(countdownDate, null, countdown.DAYS).days);
 
-  this.$toggle.on('click', this.handleToggle.bind(this));
-
+  // anonymous feedback tool click
   this.$selector.on('click', '.js-feedback', function () {
     $(document.body).trigger('feedback:open');
   });
 
+  // show more/less banner text
+  this.$toggle = this.$selector.find('.toggle-text');
+  this.$toggle.on('click', this.handleToggle.bind(this));
 }
 
-SiteOrientation.prototype.handleToggle = function() {
+SiteOrientation.prototype.handleToggle = function () {
   this.$selector.find('.toggle, .less, .more').toggle();
 };
 
